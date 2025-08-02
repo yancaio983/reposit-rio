fc764c3bc7f8d8e39e7beddcfc38f5dad6870ee1
yancaio983 <capudoff.proplayer@gmail.com>
2025-08-02 18:48:58 -0300

    arquivos

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.title("neymar goat")
+st.write("Neymar is the greatest of all time (GOAT) in football!")
+st.image("https://avenida-juarez.com/wp-content/uploads/2018/03/Neymar.jpg", caption="Neymar Jr. in action")
